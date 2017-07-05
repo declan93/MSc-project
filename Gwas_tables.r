c8bc3ad321cf2b394f49f592a0218751aace3845
declan93 <declanbennett@hotmail.com>
2017-07-05 14:52:24 +0100

    Update Gwas_tables.r

--- Gwas_tables.r
+++ Gwas_tables.r
@@ -4,7 +4,7 @@ library(xtable)
 for (i in c("KHV","GBR","CHB","CLM","CDX","MXL","CHS","GIH","JPT","PUR","ESN","FIN","ACB","ASW","YRI","ITU","PJL","TSI","PEL","LWK","MSL","STU","GWD","IBS","CEU","BEB")){
         setwd(paste("/data4/Declan/MSc_project/analysis_all_covar/",i, sep=""))
 	x <- read.table(paste(i, "_all.assoc.linear.adjusted", sep=""), h=T)
-	print(xtable(x[1:10,], type = "latex", digits=c(0,0,0,0,-2,-2,-2,-2,-2,-2,-2), file = paste(i,"_vcfassoc.tex", sep =""))
+	print(xtable(x[1:10,], type = "latex", digits=c(0,0,0,-2,-2,-2,-2,-2,-2,-2,-2), file = paste(i,"_vcfassoc.tex", sep =""))
 	rm(list=ls())
 }
 	
